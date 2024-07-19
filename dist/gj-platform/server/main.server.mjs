65b00c6ddcd998edf741a9a8a6fbe77d1eda8071
Rodolfo Mora <rodolfo.mora.zamora@gmail.com>
2024-07-18 20:55:55 -0600

    Issues in LocalOrganizer Dashboard

--- dist/gj-platform/server/main.server.mjs
+++ dist/gj-platform/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WR6RFU73.mjs";import"./chunk-TTLE45SG.mjs";import"./chunk-L6SRKSZV.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-YZFBUW7X.mjs";import"./chunk-TTLE45SG.mjs";import"./chunk-L6SRKSZV.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
