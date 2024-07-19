50f0e60c4d0b6fe16609a49cb2faacd28c327b0e
Rodolfo Mora <rodolfo.mora.zamora@gmail.com>
2024-07-18 20:44:27 -0600

    Updated frontend build

--- dist/gj-platform/server/main.server.mjs
+++ dist/gj-platform/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TONHMQZI.mjs";import"./chunk-TTLE45SG.mjs";import"./chunk-L6SRKSZV.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-66OLZ3BF.mjs";import"./chunk-TTLE45SG.mjs";import"./chunk-L6SRKSZV.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
