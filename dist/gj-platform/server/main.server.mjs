3f139514828618da7e4d41acfb549c9a47e2d790
Rodolfo Mora <rodolfo.mora.zamora@gmail.com>
2024-07-21 23:07:23 -0600

    Added dist from previous commit
    Previous commit had dist missing...

---
+++ dist/gj-platform/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-BA2JUY6B.mjs";import"./chunk-7FVVIORR.mjs";import"./chunk-IWIC5HPM.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
