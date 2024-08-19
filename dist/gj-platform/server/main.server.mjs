28da423367f95451734d74a9bba63331bd1c8671
Rjmoraza <mrfofoaustin@gmail.com>
2024-08-19 13:17:20 -0600

    Patches to Sites
    Patch in global-user-crud to alleviate delays caused by the Sites model
    Patch in the global-site-crud to diagnose problem with Sites loading

--- dist/gj-platform/server/main.server.mjs
+++ dist/gj-platform/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-EZ2V6PU2.mjs";import"./chunk-HQZIKZ6G.mjs";import"./chunk-VO22LEX5.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-4KAQYLVM.mjs";import"./chunk-HQZIKZ6G.mjs";import"./chunk-VO22LEX5.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
