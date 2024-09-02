5d95f8f1a9f22272c9d24e151454094d0a8f72ef
Rjmoraza <mrfofoaustin@gmail.com>
2024-09-02 14:09:05 -0600

    Bugfix in Global.User.Crud

--- dist/gj-platform/server/main.server.mjs
+++ dist/gj-platform/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-F27FZ6O3.mjs";import"./chunk-DIVK2OWV.mjs";import"./chunk-IK3JO7HG.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-E33JY7OM.mjs";import"./chunk-DIVK2OWV.mjs";import"./chunk-IK3JO7HG.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
