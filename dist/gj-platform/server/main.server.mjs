96b04ba4ec6f3ddde5ef4e1c607d15f1b4d835ba
Rjmoraza <mrfofoaustin@gmail.com>
2024-09-02 14:33:03 -0600

    bug in globalusercrud

--- dist/gj-platform/server/main.server.mjs
+++ dist/gj-platform/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5BWWDA36.mjs";import"./chunk-DIVK2OWV.mjs";import"./chunk-IK3JO7HG.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-GTUR2HAV.mjs";import"./chunk-DIVK2OWV.mjs";import"./chunk-IK3JO7HG.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
