665e2d03c3f20818ef050319b6eb7e53c6257b82
Rodolfo <rjmoraza@gmail.com>
2024-10-22 10:48:04 -0600

    Improvements to the UI of Submission

--- dist/gj-platform/server/main.server.mjs
+++ dist/gj-platform/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QTUHTXNN.mjs";import"./chunk-LGZH2W2I.mjs";import"./chunk-G5D7ZE25.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-7XK7CRFS.mjs";import"./chunk-LGZH2W2I.mjs";import"./chunk-G5D7ZE25.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
